9959ff1d727e578544e27e0701e79f1c30b2b8a8
roomee <roomeezon@gmail.com>
2025-08-07 07:23:23 +0500

    Delete namaz-backend directory

--- namaz-backend/utils/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const MONGO_URI = process.env.MONGO_URI;
-
-export async function connect() {
-  if (mongoose.connections[0].readyState) return;
-  await mongoose.connect(MONGO_URI);
-}
--- namaz-backend/utils/model.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const PrayerSchema = new mongoose.Schema({
-  date: String,
-  name: String,
-  status: String,
-});
-
-export default mongoose.models.Prayer || mongoose.model('Prayer', PrayerSchema);
